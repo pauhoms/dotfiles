796e81f75bd0513acc5cd27f21a7f7cb67d2752e
paupolytropo <pau@polytropo.es>
2021-08-11 15:55:41 +0200

    chrome

--- bin/repositories.sh
+++ bin/repositories.sh
@@ -8,3 +8,12 @@ sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
 sudo dnf install fedora-workstation-repositories
 sudo dnf config-manager --set-enabled google-chrome
 
+cat << EOF > sudo /etc/yum.repos.d/google-chrome.repo
+[google-chrome]
+name=google-chrome
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://dl.google.com/linux/linux_signing_key.pub
+EOF
+
