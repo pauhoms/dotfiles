c7fdd0bd34e34f347a85fc77dbecaf065aad35cd
pauhoms <pauhomsjimenez1@gmail.com>
2022-03-08 13:35:49 +0100

    fix: add -y apt commands

--- bin/docker.sh
+++ bin/docker.sh
@@ -7,8 +7,8 @@ sudo apt-get install \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
 
 sudo systemctl start docker
 sudo usermod -aG docker $USER
